1228d0af0794731971079af0f41e63b25a5ab2ff
Kitz2024 <kit.pollinger@gmail.com>
2024-10-01 19:14:16 -0700

    Lab-10 Commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
+//Kit Pollinger 
+// 210 - Lab - 10 | Fork This Replit
 #include <iostream>
 using namespace std;
 
 int main() {
 
     // add any code here, sort of a 'hello world plus' code block
+    cout << "Hello World, This is a Clone version of Lab 10" << endl;
 
     return 0;
 }
